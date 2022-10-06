f5042656170232f86a22c4c4fd6098a71b804af7
bamsi <hibamsi@gmail.com>
2022-10-07 00:46:26 +0300

    add movies relation and validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+  has_many :movies
+
+  validates :name, presence: true, length: {maximum: 250}
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,5 @@
 class Genre < ApplicationRecord
+  has_many :movies
+
+  validates :name, presence: true, length: {maximum: 250}
 end
