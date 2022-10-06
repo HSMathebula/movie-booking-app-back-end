2ac659bf9b50d3fa11de494eac72e2388b2ab979
ilham bouaik <ibouaik@gmail.com>
2022-10-06 13:49:57 +0100

    add picture colmun to movie

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_06_103207) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_06_124647) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_06_103207) do
     t.bigint "user_id", null: false
     t.bigint "category_id", null: false
     t.bigint "genre_id", null: false
+    t.string "picture"
     t.index ["category_id"], name: "index_movies_on_category_id"
     t.index ["genre_id"], name: "index_movies_on_genre_id"
     t.index ["user_id"], name: "index_movies_on_user_id"
