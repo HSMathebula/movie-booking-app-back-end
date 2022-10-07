fb13f60835e2abfd078cab3d9d9ebe8c3a844f16
bamsi <hibamsi@gmail.com>
2022-10-07 10:30:09 +0300

    add action to create a movie and delete a movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,13 +9,25 @@ class MoviesController < ApplicationController
     render json: movie.to_json 
   end
 
-  def new
-    @movie = Movie.new
-  end
-
   def create
+    movie = Movie.new(movie_params)
+    movie.user = current_user
+    if movie.save
+      render json: movie, status: 200
+    else
+      render json: {error: "Error creating a movie"}
+    end
   end
 
   def destroy
+    movie = Movie.find(params[:id])
+    if movie.destroy
+      render json: {message: "Movie deleted successfully!"}, status: 200
+    else
+      render json: {error: "Error deleting a movie"}
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :picture, :category_id, :genre_id)
   end
 end
