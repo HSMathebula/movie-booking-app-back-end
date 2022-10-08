4bed1140615a609d515412c66e7d67d06a16b208
ilham bouaik <ibouaik@gmail.com>
2022-10-08 21:14:27 +0100

    create user controller and add encode_token method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :movies
   validates :name, presence: true, uniqueness: true
   validates :password, presence: true, uniqueness: true
