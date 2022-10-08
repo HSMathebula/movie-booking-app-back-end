6d1c6f462dd485bd0238c6785f92c47bcf52cfea
ilham bouaik <ibouaik@gmail.com>
2022-10-08 22:40:07 +0100

    add decode_token method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::API
     def encode_token(payload)
         JWT.encode(payload, 'secret')
     end
+    def decode_token
+        auth_header = request.headers['Authorization']
+        if auth_header
+            token = auth_header.split(' ')[1]
+            begin
+                JWT.decode(token, 'secret', true, algorithm: 'HS256')
+            rescue JWT::DecodeError
+                nil
+            end
+        end
+
+    end
 end
