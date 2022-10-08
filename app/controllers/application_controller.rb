e096a3388daa0ea6dc8aa4f477cadb5d9de8752a
ilham bouaik <ibouaik@gmail.com>
2022-10-08 23:36:56 +0100

    fix offenses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,27 @@
 class ApplicationController < ActionController::API
-    def encode_token(payload)
-        JWT.encode(payload, 'secret')
-    end
-    def decode_token
-        auth_header = request.headers['Authorization']
-        if auth_header
-            token = auth_header.split(' ')[1]
-            begin
-                JWT.decode(token, 'secret', true, algorithm: 'HS256')
-            rescue JWT::DecodeError
-                nil
-            end
-        end
+  def encode_token(payload)
+    JWT.encode(payload, 'secret')
+  end
 
+  def decode_token
+    auth_header = request.headers['Authorization']
+    if auth_header
+      token = auth_header.split[1]
+      begin
+        JWT.decode(token, 'secret', true, algorithm: 'HS256')
+      rescue JWT::DecodeError
+        nil
+      end
     end
-    def authorized_user
-        decoded_token = decode_token()
-        if decoded_token
-            user_id = decode_token[0]["user_id"]
-            @user = User.find_by(id: user_id)
-        end
-    end
-    def authorize
-        render json: { message: "You have to log in"}, status: :unauthorized unless authorized_user
-    end
+  end
+
+  def authorized_user
+    decoded_token = decode_token
+    user_id = decode_token[0]['user_id']
+    @user = User.find_by(id: user_id) if decoded_token
+  end
+
+  def authorize
+    render json: { message: 'You have to log in' }, status: :unauthorized unless authorized_user
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,29 @@
 class UsersController < ApplicationController
-    def create
-        @user = User.create(user_params)
-        if @user.valid?
-            token = encode_token({ user_id: @user.id })
-            render json: { user: @user, token: token }, status: :ok
-        else
-            render json: { error: 'Invalid username or password' }, status: :unprocessable_entity
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      token = encode_token({ user_id: @user.id })
+      render json: { user: @user, token: }, status: :ok
+    else
+      render json: { error: 'Invalid username or password' }, status: :unprocessable_entity
 
-        end
     end
-    
-    def login
-        @user =  User.find_by(name: user_params[:name])
+  end
 
-        if @user && @user.authenticate(user_params[:password])
-            token = encode_token({ user_id: @user.id })
-            render json: { user: @user, token: token }, status: :ok
-        else
-            render json: { error: 'Invalid username or password' }, status: :unprocessable_entity
-        end
+  def login
+    @user = User.find_by(name: user_params[:name])
+
+    if @user&.authenticate(user_params[:password])
+      token = encode_token({ user_id: @user.id })
+      render json: { user: @user, token: }, status: :ok
+    else
+      render json: { error: 'Invalid username or password' }, status: :unprocessable_entity
     end
+  end
 
+  private
 
-    private
-
-    def user_params
-        params.require(:user).permit(:name, :password)
-    end
+  def user_params
+    params.require(:user).permit(:name, :password)
+  end
 end
