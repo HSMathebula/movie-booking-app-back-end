fa6a90569423c480db1565ff4077f62f87199693
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-10-10 00:00:41 +0200

    Add index, create method

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -1,6 +1,23 @@
 class Api::V1::BookingsController < ApplicationController   
   def index
     @bookings = Booking.all.order(created_at: :desc)
-    render json: @reservations, status: :ok
+    render json: @bookings, status: :ok
+  end
+
+  def show
+    render json: @booking, status: :ok
+  end
+
+  def create
+    @movie = Movies.find(params[:movie_id])
+
+    @created_booking = Booking.new(booking_params)
+    @created_booking.movie_id = @movie.id
+
+    if @created_booking.save
+      render json: @created_booking, status: :created
+    else
+      render json: { errors: @created_booking.errors.full_messages }, status: :unprocessible_entity
+    end
   end
 end
