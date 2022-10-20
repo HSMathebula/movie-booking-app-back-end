4f70903ee73bc316f0e7e1ba582aa286bec0a761
ilham bouaik <ibouaik@gmail.com>
2022-10-20 18:53:51 +0100

    add genre and category

---
+++ spec/requests/api/category_spec.rb
@@ -0,0 +1,28 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/category', type: :request do
+    path '/categories' do
+
+    get 'index' do
+      tags 'Categories'
+      consumes 'application/json'
+      parameter name: :genre, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+        },
+        required: [ 'name' ]
+      }
+
+      response '200', 'category created' do
+        let(:category) { { title: 'Comedie'} }
+        run_test!
+      end
+
+      response '422', 'invalid request' do
+        let(:category) { { title: 'Comedie' } }
+        run_test!
+      end
+    end
+  end
+end
