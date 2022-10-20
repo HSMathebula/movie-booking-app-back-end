47a836157a809a319a6039ccf2b3414c286ccd3d
ilham bouaik <ibouaik@gmail.com>
2022-10-20 11:35:47 +0100

    add category and booking models tests

--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Booking, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    before(:each) do
+    @user = User.create(name: 'test', password: 'test')
+    @category = Category.create(name: 'Horror')
+    @genre = Genre.create(name: 'Romantic')
+    @movie = Movie.create(title: 'The head', user_id: @user.id, category_id: @category.id, genre_id: @genre.id)
+  end
+
+  subject do
+    described_class.new(city: 'rabat', date: '2002/10/15', user_id: @user.id, movie_id: @movie.id)
+  end
+
+  it 'should have a valid city' do
+  subject.city = 'rabat'
+  expect(subject).to be_valid
+  end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+subject do
+    Category.create(name: "comedie")
+  end
+
+  before { subject.save }
+
+  it 'should have a valid name' do
+  subject.name = "Romantic"
+  expect(subject).to be_valid
+  end
 end
