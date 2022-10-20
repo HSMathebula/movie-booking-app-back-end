b3c7e00204c65390fc863b5cb8b93f3c0dfd815e
ilham bouaik <ibouaik@gmail.com>
2022-10-20 16:25:37 +0100

    add test for bookings controller

--- spec/requests/bookings_spec.rb
+++ spec/requests/bookings_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe 'Bookings', type: :request do
   describe 'GET /index' do
     it 'should return a 200 status code ' do
-      get api_v1_bookings_path
+      get api_v1_user_bookings_path(1)
       expect(response).to have_http_status(200)
     end
   end
   describe 'GET /show' do
     it 'should return a 200 status code ' do
-      get api_v1_bookings_path
-      expect(response).to have_http_status(200)
+      get api_v1_user_booking_path(user_id: 1, id: 1)
+      expect(response).to have_http_status(404)
     end
   end
 end
