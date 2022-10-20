d7db1a553622c27575212dcfde688b0f6ed23130
bamsi <hibamsi@gmail.com>
2022-10-20 20:56:13 +0300

    add delete and latest movie api documentation

--- spec/requests/api/movie_spec.rb
+++ spec/requests/api/movie_spec.rb
@@ -70,4 +70,61 @@ describe 'Movies API' do
       end
     end
   end
+
+  path '/movies/latest_movies/page/{page}' do
+
+    get 'Retrieves latest movies' do
+      tags 'Movies'
+      produces 'application/json', 'application/xml'
+      parameter name: :page, in: :path, type: :string
+
+      response '200', 'movie found' do
+        schema type: :object,
+          properties: {
+            title: { type: :string },
+            description: { type: :string },
+            duration: { type: :float },
+            time: { type: :datetime },
+            picture: { type: :string },
+            user_id: { type: :bigint },
+            category_id: { type: :bigint },
+            genre_id: { type: :bigint },
+          },
+          required: [ 'page' ]
+
+        let(:id) { Blog.create(title: 'the lie', description: 'sample description').id }
+        run_test!
+      end
+
+      response '404', 'movie not found' do
+        let(:id) { 'invalid' }
+        run_test!
+      end
+
+      response '406', 'unsupported accept header' do
+        let(:'Accept') { 'application/movie' }
+        run_test!
+      end
+    end
+  end
+
+  path '/movies/{id}' do
+
+    delete 'Delete a movie' do
+      tags 'Movies'
+      consumes 'application/json'
+      parameter name: :id, in: :path, type: :string
+
+      response '200', 'movie deleted' do
+        let(:movie) { { title: 'the lie', user_id: 1 , category_id: 1 , genre_id: 1 } }
+        run_test!
+      end
+
+      response '422', 'invalid request' do
+        let(:movie) { { title: 'the lie' } }
+        run_test!
+      end
+    end
+  end
+  
 end
